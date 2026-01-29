12f8cf2de788ed09d013db67ef45c30da9d5221f
Mouna199 <mouna.rhalimi@gmail.com>
2026-01-29 23:21:12 +0100

    WIP: extractors

---
+++ src/extractors/csv_extractor.py
@@ -0,0 +1,13 @@
+import polars as pl 
+from pathlib import Path
+
+def load_csv_data(file_path:str) -> pl.DataFrame:
+    """
+    Extraire les données f'un fichier CSV qui contient les données de capteurs.
+    """
+    path = Path(file_path)
+    if not path.exists(): 
+        raise FileNotFoundError(f"Le fichier {file_path} n'existe pas.")
+    df = pl.read_csv(file_path, separator=";",try_parse_dates=True)
+    return df
+
