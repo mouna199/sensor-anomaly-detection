c6253d4a2ab2b6136ad2bec9f73045089c2352e1
Mouna199 <mouna.rhalimi@gmail.com>
2026-01-30 16:02:00 +0100

    Finalize the script that transforms data extracted from csv

--- src/transformers/sensor_transformer.py
+++ src/transformers/sensor_transformer.py
@@ -30,12 +30,14 @@ def check_and_drop_duplicates(df:pl.DataFrame) -> pl.DataFrame:
     return df 
 
 
-def convert_types(df:pl.DataFrame) -> pl.DataFrame:
+def convert_types(df: pl.DataFrame) -> pl.DataFrame:
+    datetime_col = ["datetime"]
+    float_cols = ["Current", "Temperature", "Voltage", "Accelerometer1RMS",
+                  "Accelerometer2RMS", "Volume Flow RateRMS", "Pressure", "Thermocouple"]
+    
     return df.with_columns([
-        pl.col('datetime').str.to_datetime(), 
-        pl.col('Current', 'Temperature', 'Accelerometer1RMS', 
-               'Accelerometer2RMS', 'Flow', 'Volume', 
-               'RateRMS', 'Pressure', 'Thermocouple').cast(pl.Float64)
+        pl.col("datetime").cast(pl.Datetime),
+        pl.col(float_cols).cast(pl.Float64)
     ])
 
 
@@ -57,3 +59,15 @@ def transform_dataframe(df:pl.DataFrame) -> pl.DataFrame:
           )
     logger.info(f"Fin transformation: {len(df)} lignes")
     return df
+#df_test = pl.DataFrame({
+#    "datetime": ["2019-07-08 17:02:14"] * 9,
+#    "id": ["Current", "Temperature", "Accelerometer1RMS", 
+#           "Accelerometer2RMS", "Flow", "Volume", 
+#           "RateRMS", "Pressure", "Thermocouple"],
+#    "value": [0.000749, 28.2099, 0.0428296, 
+#              0.0806121, 10.0, 5.0, 
+#              36.0334, -0.601143, 24.3337],
+#    "index": [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#})
+##df = transform_dataframe(df_test)
+##print(df)
